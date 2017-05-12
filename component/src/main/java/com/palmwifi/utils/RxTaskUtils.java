a0c81b16d8b79c3a08d894892cd29eef1763aded
David <david@mewifi.com.cn>
2017-05-12 10:10:07 +0800

    增加RxTaskUtils

--- component/src/main/java/com/palmwifi/utils/RxTaskUtils.java
+++ component/src/main/java/com/palmwifi/utils/RxTaskUtils.java
@@ -26,8 +26,11 @@ public class RxTaskUtils {
      * @param provider 绑定对象
      * @param action1 执行任务
      */
-    public static<T> void sendMain(LifecycleProvider<T> provider,Action1<Long> action1){
-        delayMain(0,provider,action1);
+    public static<T> void sendMain(LifecycleProvider<T> provider,Action1<Object> action1){
+        Observable.empty()
+                .observeOn(AndroidSchedulers.mainThread())
+                .compose(provider.bindToLifecycle())
+                .subscribe(action1);
     }
 
     /**
@@ -35,8 +38,11 @@ public class RxTaskUtils {
      * @param provider 绑定对象
      * @param action1 执行任务
      */
-    public static<T> void sendAnsyc(LifecycleProvider<T> provider,Action1<Long> action1){
-        delayAsync(0,provider,action1);
+    public static<T> void sendAnsyc(LifecycleProvider<T> provider,Action1<Object> action1){
+        Observable.empty()
+                .observeOn(Schedulers.newThread())
+                .compose(provider.bindToLifecycle())
+                .subscribe(action1);
     }
 
     /**
@@ -48,7 +54,6 @@ public class RxTaskUtils {
     public static<T> void delayMain(int delay, LifecycleProvider<T> provider,Action1<Long> action1){
         Observable.timer(delay, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
                 .compose(provider.<Long>bindToLifecycle())
                 .subscribe(action1);
     }
@@ -62,8 +67,7 @@ public class RxTaskUtils {
     public static<T> void delayAsync(int delay, LifecycleProvider<T> provider,Action1<Long> action1){
 
         Observable.timer(delay, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.io())
-                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.newThread())
                 .compose(provider.<Long>bindToLifecycle())
                 .subscribe(action1);
     }
@@ -75,10 +79,10 @@ public class RxTaskUtils {
      * @param provider 绑定对象
      * @param action1 执行任务
      */
-    public static<T> void intervalMain(int interval, LifecycleProvider<T> provider,Action1<Long> action1){
+    public static<T> void intervalMain(int interval, int count,LifecycleProvider<T> provider,Action1<Long> action1){
         Observable.interval(interval, TimeUnit.MILLISECONDS)
+                .take(count)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
                 .compose(provider.<Long>bindToLifecycle())
                 .subscribe(action1);
     }
@@ -89,62 +93,16 @@ public class RxTaskUtils {
      * @param provider 绑定对象
      * @param action1 执行任务
      */
-    public static<T> void intervalAsync(int interval, LifecycleProvider<T> provider,Action1<Long> action1){
-
-        Observable.interval(interval, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.io())
-                .subscribeOn(Schedulers.io())
-                .compose(provider.<Long>bindToLifecycle())
-                .subscribe(action1);
-    }
-
-    /**
-     * 循环发送到主线程
-     * @param interval 时间毫秒
-     * @param count 执行次数
-     * @param provider 绑定对象
-     * @param action1 执行任务
-     */
-    public static<T> void intervalMain(int interval, int count,LifecycleProvider<T> provider,Action1<Long> action1){
-
-        final Subscription subscription = Observable.interval(interval, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .compose(provider.<Long>bindToLifecycle())
-                .subscribe(action1);
-        delayMain(count*interval, provider, new Action1<Long>() {
-            @Override
-            public void call(Long aLong) {
-                if(subscription != null && subscription.isUnsubscribed()){
-                    subscription.unsubscribe();
-                }
-            }
-        });
-    }
-
-    /**
-     * 循环发送到主线程
-     * @param interval 时间毫秒
-     * @param count 执行次数
-     * @param provider 绑定对象
-     * @param action1 执行任务
-     */
     public static<T> void intervalAsync(int interval, int count,LifecycleProvider<T> provider,Action1<Long> action1){
 
-        final Subscription subscription = Observable.interval(interval, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.io())
-                .subscribeOn(Schedulers.io())
+        Observable.interval(interval, TimeUnit.MILLISECONDS)
+                .take(count)
+                .observeOn(Schedulers.newThread())
                 .compose(provider.<Long>bindToLifecycle())
                 .subscribe(action1);
-        delayAsync(count*interval, provider, new Action1<Long>() {
-            @Override
-            public void call(Long aLong) {
-                if(subscription != null && subscription.isUnsubscribed()){
-                    subscription.unsubscribe();
-                }
-            }
-        });
     }
+
+
 
 
     /**
@@ -163,4 +121,5 @@ public class RxTaskUtils {
             throw new IllegalArgumentException("view isn't activity or fragment");
         }
     }
+
 }
--- component/src/main/java/com/palmwifi/utils/RxViewUtils.java
+++ component/src/main/java/com/palmwifi/utils/RxViewUtils.java
@@ -4,40 +4,67 @@ import android.view.View;
 
 import com.jakewharton.rxbinding.view.RxView;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by Kiki on 2017/1/17.
  */
 
 public class RxViewUtils {
-    //防止多次点击
-    public static void setView(View view, Action1 onNext) {
+    /**
+     * 防止多次点击
+     * @param view
+     * @param onNext
+     */
+    public static void setOnClick(View view, Action1 onNext) {
         RxView.clicks(view)
                 .throttleFirst(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(onNext);
     }
 
-    public static void setViewForRecord(View view, final Action1 onNext) {
 
-        RxView.clicks(view)
-                .throttleFirst(1, TimeUnit.SECONDS)
-                .subscribe(onNext);
+    /**
+     * 统计点击多少次数
+     * @param view
+     * @param timeout
+     * @param action
+     */
+    public static void setCountClick(View view,long timeout,Action1<List<Void>> action){
+        Observable<Void> observable = RxView.clicks(view).share();
+        observable.buffer(observable.debounce(400, TimeUnit.MILLISECONDS))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(action);
     }
 
-    public static void setView(View view, Action1 onNext, Action1 onError) {
-        RxView.clicks(view)
-                .throttleFirst(1, TimeUnit.SECONDS)
-                .subscribe(onNext, onError);
-    }
-
-    public static void setView(View view, Action1 onNext, Action1 onError, Action0 onCompeleted) {
-        RxView.clicks(view)
-                .throttleFirst(1, TimeUnit.SECONDS)
-                .subscribe(onNext, onError, onCompeleted);
+    /**
+     * 倒计时
+     * @param time
+     * @param startAction
+     * @param subscriber
+     */
+    public static void countdown(int time,Action0 startAction,Subscriber<Integer> subscriber){
+        if (time < 0) time = 0;
+        final int countTime = time;
+         Observable.interval(0, 1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<Long, Integer>() {
+                    @Override
+                    public Integer call(Long increaseTime) {
+                        return countTime - increaseTime.intValue();
+                    }
+                })
+                .take(countTime + 1).doOnSubscribe(startAction)
+                .subscribe(subscriber);
     }
 
 }
